80ac416214edc24d3efae8e73fdee19603672f55
Bobby Lee <thayungbobby@gmail.com>
2022-11-08 21:21:15 -0500

    added serializer for cars_list

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
 @api_view(['GET'])
 def cars_list(request):
-    
+    cars = Car.objects.all()
 
-    return Response('okay')
+    serializer = CarSerializer(cars, many=True) # because we are using an all query(which would have many cars), set many to True
+
+    return Response(serializer.data) # returns the data from the serializer as a response
