3fd7636aaab684cadfbe34d7d114972d45319e72
Bobby Lee <thayungbobby@gmail.com>
2022-11-08 22:35:16 -0500

    updated urls

--- cars/urls.py
+++ cars/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cars_list),
-    path('<pk>/', views.car_detail), # these arrows brackets help pass the value through
+    path('<int:pk>/', views.car_detail), # these arrows brackets help pass the value through
 ]
