654008a07f011f198b8f718640a98b6a4f9bd67b
Bobby Lee <thayungbobby@gmail.com>
2022-11-08 22:12:23 -0500

    added POST function

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def cars_list(request):
-    cars = Car.objects.all()
 
-    serializer = CarSerializer(cars, many=True) # because we are using an all query(which would have many cars), set many to True
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True) # because we are using an all query(which would have many cars), set many to True
+        return Response(serializer.data, status=status.HTTP_200_OK) # returns the data from the serializer as a response
 
-    return Response(serializer.data) # returns the data from the serializer as a response
+
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED) # returns the saved data and gives a 201 HTTP status
+        
