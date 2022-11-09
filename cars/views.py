85dbd7662151072d84a2c46bc18607f19a45e35c
Bobby Lee <thayungbobby@gmail.com>
2022-11-08 22:21:53 -0500

    added car_detail function

--- cars/views.py
+++ cars/views.py
@@ -11,11 +11,14 @@ def cars_list(request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True) # because we are using an all query(which would have many cars), set many to True
         return Response(serializer.data, status=status.HTTP_200_OK) # returns the data from the serializer as a response
-
-
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED) # returns the saved data and gives a 201 HTTP status
-        
+
+@api_view(['GET'])
+def car_detail(request, pk):
+
+    print(pk)
+    return Response(pk)
